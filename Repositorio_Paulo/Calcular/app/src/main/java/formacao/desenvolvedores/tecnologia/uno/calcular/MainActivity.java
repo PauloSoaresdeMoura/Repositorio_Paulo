01abe6b644ddc5c403df3f9852da13594421a1e2
PauloSoaresdeMoura <paulo77moura@gmail.com>
2022-06-02 11:22:15 -0300

    novo projeto

--- Repositorio_Paulo/Calcular/app/src/main/java/formacao/desenvolvedores/tecnologia/uno/calcular/MainActivity.java
+++ Repositorio_Paulo/Calcular/app/src/main/java/formacao/desenvolvedores/tecnologia/uno/calcular/MainActivity.java
@@ -3,12 +3,23 @@ package formacao.desenvolvedores.tecnologia.uno.calcular;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvOpcao, tv1, tv2, tv3;
+    private Spinner spiOpcoes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvOpcao = findViewById(R.id.tvOpcao);
+        spiOpcoes = findViewById(R.id.spiOpcoes);
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
+        tv3 = findViewById(R.id.tv3);
     }
 }
