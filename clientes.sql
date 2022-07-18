9bd76e69d962dc3191476d84205fdefceba71225
PauloSoaresdeMoura <paulo77moura@gmail.com>
2022-07-18 19:58:33 -0300

    clientes

---
+++ clientes.sql
@@ -0,0 +1,10 @@
+/*INSERT INTO cliente (nome, telefonefixo, celular, endereco, numero,  bairro, cep)
+VALUES ('Maria', '3323-9874', '98896-2501', 'Rua Sete de Setembro', '89', 'Perdizes', '52320568');*/
+
+/*INSERT INTO cliente (nome, telefonefixo, celular, endereco, numero,  bairro, cep)
+VALUES ('José', '3321-9857', '', 'Av. São Pedro', '114', 'São Cristovão.', '18524021');*/
+
+/*clienteINSERT INTO cliente (nome, telefonefixo, celular, endereco, numero,  bairro, cep)
+VALUES ('Marcio', '3323-5847', '', 'Av. Atílio Fontana', '321', 'Efapi', '47820321');*/
+
+
