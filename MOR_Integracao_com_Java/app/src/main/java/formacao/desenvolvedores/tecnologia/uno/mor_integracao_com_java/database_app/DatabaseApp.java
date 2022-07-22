6374518176aab1ba73df9cdbc482109e86ba39d3
PauloSoaresdeMoura <paulo77moura@gmail.com>
2022-07-22 11:25:45 -0300

    novo banco

---
+++ MOR_Integracao_com_Java/app/src/main/java/formacao/desenvolvedores/tecnologia/uno/mor_integracao_com_java/database_app/DatabaseApp.java
@@ -0,0 +1,46 @@
+package formacao.desenvolvedores.tecnologia.uno.mor_integracao_com_java.database_app;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public abstract class DatabaseApp extends RoomDatabase {
+    private static final String DB_NAME = "db_app.db";//nome do banco de dados que será criado
+    private static volatile DatabaseApp sInstance;
+
+    public static final String TAG = "DatabaseApp";
+    public DatabaseApp() {//construtor público vazio
+    }
+
+    public static synchronized DatabaseApp getInstance(Context context){
+        return null;
+    }
+
+    private static DatabaseApp getConnection(Context contextActivity){
+        return null;
+    }
+
+    static RoomDatabase.Callback initDB = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate (@NonNull SupportSQLiteDatabase db){
+            super.onOpen(db);
+            Executors.newSingleThreadScheduledExecutor().execute(() -> {
+                Log.d(TAG , "Banco criado com sucesso! Executar rotinas posteriores.");
+            });
+        }
+
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+        }
+
+    };
+
+
+}
